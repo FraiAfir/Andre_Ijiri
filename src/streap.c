5b33dd5b1553e37bc70ce3bbd7b4f6526ad4508e
André Ijiri Ribeiro <andre.ijiri.ribeiro@gmail.com>
2025-10-13 13:38:06 -0300

    Comecei a implementação do TAD STreap

---
+++ src/streap.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "streap.h"
+
+/*                            ESTRUTURA DE DADOS A SER IMPLEMENTADA                            */
+// Estruturas para a árvore treap espacial
+typedef struct streap{
+    Node*  raiz;     // Raiz da árvore
+    double epsilon; // Precisão para comparar números reais
+}STreap;
+// Estruturas para a informação, nó e lista de nós
+typedef struct info{
+    char*  descricao; // Descrição da informação
+    double valor;    // Valor associado à informação
+}Info;
+// Nó da árvore
+typedef struct node{
+    double x, y;       // Coordenadas da âncora
+    int    prioridade; // Prioridade do nó (para manter a propriedade de heap da treap)
+    Info*  info;       // Informação associada ao nó
+
+    double mbbX1, mbbY1, mbbX2, mbbY2; // Bounding box do nó (retângulo envolvente)
+
+    Node*  esq; // Filho esquerdo
+    Node*  dir; // Filho direito
+}Node;
+// Lista de nós
+typedef struct lista{
+    Node** nos;        // Array de nós
+    int    tamanho;    // Tamanho atual da lista
+    int    capacidade; // Capacidade máxima da lista
+}Lista;
+/*#############################################################################################*/
+
+/*                                      FUNÇÕES AUXILIARES                                     */
+/*#############################################################################################*/
+
+/*                                      FUNÇÕES PRINCIPAIS                                     */
+/*#############################################################################################*/
