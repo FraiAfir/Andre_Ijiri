fe3ab9a0b8aae946adb655d72fc5e31894efe0e3
André Ijiri Ribeiro <andre.ijiri.ribeiro@gmail.com>
2025-11-17 13:05:20 -0300

    Comentado
    Não estou utilizando no momento

--- src/lista.c
+++ src/lista.c
@@ -1,85 +1,85 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
 
-#include "lista.h"
+// #include "lista.h"
 
-/*                           ESTRUTURAS DE DADOS A SEREM IMPLEMENTADAS                           */
-// Estrutura para a lista encadeada
-typedef struct lista{
-    Celula* inicio;
-    Celula* fim;
-    int tamanho;
-}Lista;
-// Estrutura para a célula da lista
-typedef struct cel{
-    void* item;
-    struct cel* prox;
-    struct cel* ant;
-}Celula;
-/*###############################################################################################*/
+// /*                           ESTRUTURAS DE DADOS A SEREM IMPLEMENTADAS                           */
+// // Estrutura para a lista encadeada
+// typedef struct lista{
+//     Celula* inicio;
+//     Celula* fim;
+//     int tamanho;
+// }Lista;
+// // Estrutura para a célula da lista
+// typedef struct cel{
+//     void* item;
+//     struct cel* prox;
+//     struct cel* ant;
+// }Celula;
+// /*###############################################################################################*/
 
 
 
-/*                                       FUNÇÕES PRINCIPAIS                                      */
-// Função para criar uma lista encadeada vazia
-Lista* criarLista(){
-    Lista* l = (Lista*)malloc(sizeof(Lista));
+// /*                                       FUNÇÕES PRINCIPAIS                                      */
+// // Função para criar uma lista encadeada vazia
+// Lista* criarLista(){
+//     Lista* l = (Lista*)malloc(sizeof(Lista));
 
-    l->inicio  = NULL;
-    l->fim     = NULL;
-    l->tamanho = 0;
+//     l->inicio  = NULL;
+//     l->fim     = NULL;
+//     l->tamanho = 0;
 
-    return l;
-}
-// Função para inserir um item no final da lista
-void inserirFim(Lista* lista, void* v){
-    Celula* nova = malloc(sizeof(Celula));
-    nova->item = v;
+//     return l;
+// }
+// // Função para inserir um item no final da lista
+// void inserirFim(Lista* lista, void* v){
+//     Celula* nova = malloc(sizeof(Celula));
+//     nova->item = v;
 
-    if (lista->inicio == NULL){
-        lista->inicio = nova;
-        lista->fim    = nova;
+//     if (lista->inicio == NULL){
+//         lista->inicio = nova;
+//         lista->fim    = nova;
 
-        nova->prox = NULL;
-        nova->ant  = NULL;
-    }else{
-        nova->prox = NULL;
-        lista->fim->prox = nova;
-        nova->ant        = lista->fim;
-        lista->fim       = nova;
-    }
+//         nova->prox = NULL;
+//         nova->ant  = NULL;
+//     }else{
+//         nova->prox = NULL;
+//         lista->fim->prox = nova;
+//         nova->ant        = lista->fim;
+//         lista->fim       = nova;
+//     }
 
-    lista->tamanho += 1;
-}
-// Função para remover o item do início da lista
-void removerInicio(Lista* lista){
-    Celula* cel = lista->inicio;
+//     lista->tamanho += 1;
+// }
+// // Função para remover o item do início da lista
+// void removerInicio(Lista* lista){
+//     Celula* cel = lista->inicio;
 
-    lista->inicio = lista->inicio->prox;
-    if(lista->inicio == NULL) lista->fim = NULL; // Lista ficará vazia
-    else                      lista->inicio->ant = NULL;
+//     lista->inicio = lista->inicio->prox;
+//     if(lista->inicio == NULL) lista->fim = NULL; // Lista ficará vazia
+//     else                      lista->inicio->ant = NULL;
     
-    free(cel);
-    lista->tamanho -= 1;
-}
-// Função para limpar a lista e desalocar todos os recursos
-void limparLista(Lista* lista) {
-    if (!lista) return;
+//     free(cel);
+//     lista->tamanho -= 1;
+// }
+// // Função para limpar a lista e desalocar todos os recursos
+// void limparLista(Lista* lista) {
+//     if (!lista) return;
 
-    Celula* atual = lista->inicio;
-    while (atual != NULL) {
-        Celula* temp = atual;
-        atual = atual->prox;
+//     Celula* atual = lista->inicio;
+//     while (atual != NULL) {
+//         Celula* temp = atual;
+//         atual = atual->prox;
 
-        free(temp->item); // Libera o item armazenado
-        free(temp);       // Libera a célula
-    }
+//         free(temp->item); // Libera o item armazenado
+//         free(temp);       // Libera a célula
+//     }
 
-    lista->inicio = NULL;
-    lista->fim = NULL;
-    lista->tamanho = 0;
+//     lista->inicio = NULL;
+//     lista->fim = NULL;
+//     lista->tamanho = 0;
 
-    free(lista);
-}
-/*###############################################################################################*/
+//     free(lista);
+// }
+// /*###############################################################################################*/
--- src/lista.h
+++ src/lista.h
@@ -1,34 +1,34 @@
-#ifndef _Lista_H
-#define _Lista_H
+// #ifndef _Lista_H
+// #define _Lista_H
 
-typedef struct Lista Lista;
-typedef struct cel   Cel;
+// typedef struct Lista Lista;
+// typedef struct cel   Cel;
     
-/*                                       FUNÇÕES PRINCIPAIS                                      */
-/**
- * Cria uma lista encadeada vazia
- * @return Ponteiro para a lista criada. NULL, se não conseguiu criar a lista
- */
-Lista* criaLista();
-/**
- * Insere um item v no final da lista
- * @param lista A lista onde o item será inserido
- * @param v O item a ser inserido
- * @return Void
- */
-void inserirFim(Lista* lista, void* v);
-/**
- * Remove o item do início da lista
- * @param lista A lista onde o item será removido
- * @return Void
- */
-void removerInicio(Lista* lista);
-/**
- * Libera todos os recursos usados pela lista
- * @param lista A lista a ser liberada
- * @return Void
- */
-void limparLista(Lista* lista);
-/*###############################################################################################*/
+// /*                                       FUNÇÕES PRINCIPAIS                                      */
+// /**
+//  * Cria uma lista encadeada vazia
+//  * @return Ponteiro para a lista criada. NULL, se não conseguiu criar a lista
+//  */
+// Lista* criaLista();
+// /**
+//  * Insere um item v no final da lista
+//  * @param lista A lista onde o item será inserido
+//  * @param v O item a ser inserido
+//  * @return Void
+//  */
+// void inserirFim(Lista* lista, void* v);
+// /**
+//  * Remove o item do início da lista
+//  * @param lista A lista onde o item será removido
+//  * @return Void
+//  */
+// void removerInicio(Lista* lista);
+// /**
+//  * Libera todos os recursos usados pela lista
+//  * @param lista A lista a ser liberada
+//  * @return Void
+//  */
+// void limparLista(Lista* lista);
+// /*###############################################################################################*/
 
-#endif
+// #endif
