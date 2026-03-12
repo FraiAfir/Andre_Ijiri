90d42246a7b31471b2b211950aa8cceaa797bb80
André Ijiri Ribeiro <andre.ijiri.ribeiro@gmail.com>
2026-03-12 09:31:52 -0300

    Updated main
    Corrigi as relações de ponteiro

--- src/main.c
+++ src/main.c
@@ -16,17 +16,17 @@ int main(int argc, char* argv[]){
     for(int i = 0; i < argc; i++) {printf("Argumento do argv[%d]: %s\n", i, argv[i]);}
 
     // 1: Cria os objetos das estruturas necssárias para a execução do código
-    Param* param = NULL;              // Declara o objeto de parâmetros
-    Lista* l     = NULL;              // Declara o objeto da ED para armazenar os dados dos arquivos de entrada
-    Geo*   geo   = NULL;              // Declara o objeto para armazenar os dados de uma das linha do arquivo .geo
-    Qry*   qry   = NULL;              // Declara o objeto para armazenar os dados de uma das linha do arquivo .qry (Se necessário)
-    bootProgram(param, l, geo, qry);  // Inicializa os objetos declarados acima
+    Param* param = NULL;                 // Declara o objeto de parâmetros
+    Lista* l     = NULL;                 // Declara o objeto da ED para armazenar os dados dos arquivos de entrada
+    Geo*   geo   = NULL;                 // Declara o objeto para armazenar os dados de uma das linha do arquivo .geo
+    Qry*   qry   = NULL;                 // Declara o objeto para armazenar os dados de uma das linha do arquivo .qry (Se necessário)
+    bootProgram(&param, &l, &geo, &qry); // Inicializa os objetos declarados acima
 
     // 2. PROCESSAR PARÂMETROS DA LINHA DE COMANDOS
     printf("\n#------------ PROCESSANDO OS PARAMETROS DA LINHA DE COMANDO... ----------#\n");
     if(processarParametros(param, argc, argv) == -1) {
         printf("ERRO: Processamento dos parametros da linha de comando.\n");
-        shutProgram(param, l, geo, qry);
+        shutProgram(&param, &l, &geo, &qry);
         return -1;
     }
     printf("#------------------------------------------------------------------------#\n\n");
@@ -37,7 +37,7 @@ int main(int argc, char* argv[]){
     printf("\n#-------------------- PROCESSANDO O ARQUIVO .GEO... ---------------------#\n");
     if(processarGeo(param, l) == -1){
         printf("ERRO: Processamento do arquivo .geo.\n");
-        shutProgram(param, l, geo, qry);
+        shutProgram(&param, &l, &geo, &qry);
         return -1;
     }
     printf("#------------------------------------------------------------------------#\n\n");
@@ -65,7 +65,7 @@ int main(int argc, char* argv[]){
         printf("\n#-------------------- PROCESSANDO O ARQUIVO .QRY... ---------------------#\n");
         if(processarQry(param) == -1){
             printf("ERRO: Processamento do arquivo .qry.\n");
-            shutProgram(param, l, geo, qry);
+            shutProgram(&param, &l, &geo, &qry);
             return -1;
         }
         printf("#------------------------------------------------------------------------#\n\n");
@@ -75,7 +75,7 @@ int main(int argc, char* argv[]){
     printf("\n#------------------- GERANDO OS ARQUIVOS DE SAIDA... --------------------#\n");
     if(processarSvg(param) == -1){
         printf("ERRO: Geracao dos arquivos de saida.\n");
-        shutProgram(param, l, geo, qry);
+        shutProgram(&param, &l, &geo, &qry);
         return -1;
     }
     // if(processarTxt(param) == -1){
@@ -87,7 +87,7 @@ int main(int argc, char* argv[]){
 
     // 6. LIBERAR MEMÓRIA ALOCADA PARA PARÂMETROS E ENCERRAR PROGRAMA
     // Implementar a função de liberação de memória para os objetos de Param, da ED utilizada, do Geo e do Qry    
-    shutProgram(param, l, geo, qry);
+    shutProgram(&param, &l, &geo, &qry);
     printf("\n##################### FIM DA EXECUCAO DO PROGRAMA ########################\n\n");
 
     return 0;
