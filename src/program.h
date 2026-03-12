d5b76a0581ec858f93e17d3b4cd805184080dcbe
André Ijiri Ribeiro <andre.ijiri.ribeiro@gmail.com>
2026-03-12 09:32:32 -0300

    Updated program
    Corrigi as relações de ponteiro

--- src/program.h
+++ src/program.h
@@ -19,14 +19,14 @@
  * @param l     Ponteiro para o objeto criado da ED
  * @return      0 em caso de sucesso. -1 em caso de erro
  */
-int bootProgram(Param* param, Lista* l, Geo* geo, Qry* qry);
+int bootProgram(Param** param, Lista** l, Geo** geo, Qry** qry);
 /**
  * Função para liberar a memória alocada para os objetos de Parametro e da ED
  * @param param Ponteiro para o objeto de Parametro
  * @param l     Ponteiro para o objeto da ED
  * @return      0 em caso de sucesso. -1 em caso de erro
  */
-int shutProgram(Param* param, Lista* l, Geo* geo, Qry* qry);
+int shutProgram(Param** param, Lista** l, Geo** geo, Qry** qry);
 /*###############################################################################################*/
 
 #endif
