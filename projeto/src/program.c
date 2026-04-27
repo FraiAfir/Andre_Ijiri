c945107c4c49c51e193e024659d808ffe098cd68
André Ijiri Ribeiro <andre.ijiri.ribeiro@gmail.com>
2026-04-27 11:26:47 -0300

    Adaptação
    Adaptei o TAD program para trabalhar com o hashfile de pessoas

--- projeto/src/program.c
+++ projeto/src/program.c
@@ -22,7 +22,7 @@
 
 
 /*                                       FUNÇÕES PRINCIPAIS                                      */
-int bootProgram(Param** param, TabelaHash** h, Quadras** q){
+int bootProgram(Param** param, TabelaHash** ht, Quadras** q, hashPM** hp, Pessoas** p){
     printf("Inicilizando os objetos e alocando os recursos necessarios:\n");
 
     // 1: Cria o objeto de parâmetros para armazenar os dados dos argumentos da linha de comando
@@ -30,7 +30,7 @@ int bootProgram(Param** param, TabelaHash** h, Quadras** q){
     *param = criarParametro();
     if(*param == NULL){
         fprintf(stderr, "ERRO: Falha na alocacao de memoria para o objeto Parametro\n");
-        shutProgram(param, NULL, NULL);
+        shutProgram(param, NULL, NULL, NULL, NULL);
         return -1;
     } fprintf(stdout, "\tObjeto de Parametro criado com sucesso para armazenar os dados dos argumentos da linha de comando\n");
 
@@ -38,38 +38,67 @@ int bootProgram(Param** param, TabelaHash** h, Quadras** q){
 
     // 2: Cria a estrutura de dados necessária para armazenar os dados do arquivo .geo
     printf("\tCriando a Tabela Hash para armazenar os dados do arquivo .geo...\n");
-    *h = criarHash("quadras.hf");
-    if(*h == NULL){
+    *ht = criarHash("quadras.hf");
+    if(*ht == NULL){
         fprintf(stderr, "ERRO: Criar a estrutura de dados para armazenar os dados do arquivo .geo.\n");
-        shutProgram(param, h, NULL);
+        shutProgram(param, ht, NULL, NULL, NULL);
         return -1;
     } fprintf(stdout, "\tTabela Hash criada com sucesso para armazenar os dados do arquivo .geo\n");
 
     printf("\n");
 
-    // 3: Criar o objeto para armazenar os dados lidos do arquivo .geo
+    // 3: Cria a estrutura de dados necessária para armazenar os dados do arquivo .pm
+    printf("\tCriando a Tabela Hash para armazenar os dados do arquivo .pm...\n");
+    *hp = criarHashPM("pessoas.hf");
+    if(*hp == NULL){
+        fprintf(stderr, "ERRO: Criar a estrutura de dados para armazenar os dados do arquivo .pm.\n");
+        shutProgram(param, ht, NULL, hp, NULL);
+        return -1;
+    } fprintf(stdout, "\tTabela Hash criada com sucesso para armazenar os dados do arquivo .pm\n");
+
+    printf("\n");
+
+    // 4: Criar o objeto para armazenar os dados lidos do arquivo .geo
     printf("\tCriando o objeto de Quadras para armazenar os dados do arquivo .geo...\n");
     *q = criarQuadra();
     if(*q == NULL){
         fprintf(stderr, "ERRO: Falha na alocacao de memoria para o objeto Quadras\n");
-        shutProgram(param, h, q);
+        shutProgram(param, ht, q, hp, NULL);
         return -1;
     } fprintf(stdout, "\tObjeto de Quadras criado com sucesso para armazenar os dados do arquivo .geo\n");
+
+    printf("\n");
+
+    // 5: Criar o objeto para armazenar os dados lidos do arquivo .pm
+    printf("\tCriando o objeto de Pessoas para armazenar os dados do arquivo .pm...\n");
+    *p = criarPessoa();
+    if(*p == NULL){
+        fprintf(stderr, "ERRO: Falha na alocacao de memoria para o objeto Pessoas\n");
+        shutProgram(param, ht, q, hp, p);
+        return -1;
+    } fprintf(stdout, "\tObjeto de Pessoas criado com sucesso para armazenar os dados do arquivo .pm\n");
 
     printf("Objetos e recursos alocados com sucesso!\n");
     return 0;
 }
-int shutProgram(Param** param, TabelaHash** h, Quadras** q){
+
+int shutProgram(Param** param, TabelaHash** ht, Quadras** q, hashPM** hp, Pessoas** p){
     printf("Encerrando o programa e liberando os recursos alocados...\n");
 
     // 1: Libera a memória alocada para os objetos de Parametro
     freeParametros(*param);
 
     // 2: Libera a estrutura de dados utilizada
-    freeHash(*h);
+    freeHash(*ht);
 
     // 3: Libera a memória alocada para o objeto de Quadras
     freeQuadra(*q);
+
+    // 4: Libera a estrutura de dados utilizada para armazenar os dados do arquivo .pm
+    freeHashPM(*hp);
+
+    // 5: Libera a memória alocada para o objeto de Pessoas
+    freePessoa(*p);
 
     printf("\nPrograma encerrado com sucesso!\n");
     return 0;
--- projeto/src/program.h
+++ projeto/src/program.h
@@ -8,6 +8,7 @@
 #include "qry.h"
 #include "params.h"
 #include "hashTable.h"
+#include "hashPM.h"
 
 /*
     O programa é responsável por inicializar as estruturas de dados necessárias para a execução do código,
@@ -34,19 +35,23 @@
 /**
  * Esta função é responsável por inicializar os objetos de Parametro e da TabelaHash para armazenar os dados do arquivo .geo
  * @param param Ponteiro para o objeto criado de Parametro
- * @param h     Ponteiro para o objeto criado da TabelaHash
+ * @param ht    Ponteiro para o objeto criado da TabelaHash
  * @param q     Ponteiro para o objeto criado para armazenar os dados lidos do arquivo .geo
+ * @param hp    Ponteiro para o objeto criado da TabelaHash para armazenar os dados do arquivo .pm
+ * @param p     Ponteiro para o objeto criado para armazenar os dados lidos do arquivo .pm
  * @return      0 em caso de sucesso. -1 em caso de erro
  */
-int bootProgram(Param** param, TabelaHash** h, Quadras** q);
+int bootProgram(Param** param, TabelaHash** ht, Quadras** q, hashPM** hp, Pessoas** p);
 /**
- * Esta função é responsável por liberar a memória alocada para os objetos de Parametro e da TabelaHash
+ * Esta função é responsável por liberar a memória alocada para os objetos de Parametro e da Tabela Hash
  * @param param Ponteiro para o objeto de Parametro
- * @param h     Ponteiro para o objeto da TabelaHash
+ * @param ht    Ponteiro para o objeto da TabelaHash
  * @param q     Ponteiro para o objeto de Quadras
+ * @param hp    Ponteiro para o objeto da TabelaHash para armazenar os dados do arquivo .pm
+ * @param p     Ponteiro para o objeto de Pessoas
  * @return      0 em caso de sucesso. -1 em caso de erro
  */
-int shutProgram(Param** param, TabelaHash** h, Quadras** q);
+int shutProgram(Param** param, TabelaHash** ht, Quadras** q, hashPM** hp, Pessoas** p);
 
 /*###############################################################################################*/
 
