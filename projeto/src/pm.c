1066f30caf47ff8bd2e7659199bf54265572cc4f
André Ijiri Ribeiro <andre.ijiri.ribeiro@gmail.com>
2026-05-02 13:22:09 -0300

    Correção
    Consertei o parsing do arquivo .pm

--- projeto/src/pm.c
+++ projeto/src/pm.c
@@ -5,24 +5,29 @@
 #include "pm.h"
 #include "params.h"
 #include "hashPM.h"
+#include "hashTable.h"
 
 /*                                       FUNÇÕES AUXILIARES                                      */
 int montarCaminhoPM(Param* param, char* caminhoPM){
+    // 1: Monta o caminho completo do arquivo .pm a partir do diretório de entrada 
+    // e do nome do arquivo .pm fornecidos como parâmetros
     char* dirEntrada = getDirEntradaCompleto(param);
     char* nomePM     = getNomePM(param);
 
-    // Imprime o nome do arquivo .pm original para depuração
+    // 2: Imprime o nome do arquivo .pm original para depuração
     printf("Arquivo .pm fornecido: \t\t\t%s\n", nomePM);
     
-    // Concatena o diretório de entrada completo com o nome do arquivo .pm
+    // 3: Concatena o diretório de entrada completo com o nome do arquivo .pm
     strcpy(caminhoPM, dirEntrada); // Copia o diretório de entrada completo
     strcat(caminhoPM, nomePM);     // Concatena o nome do arquivo .pm ao caminho completo do diretório de entrada
+
+    // 4: Imprime o caminho completo do arquivo .pm para depuração
     printf("Caminho completo do arquivo .pm: \t%s\n", caminhoPM);
 
     return 0;
 }
 
-int readFilePM(FILE* arquivoPM, hashPM* dir, Pessoas* p){
+int readFilePM(FILE* arquivoPM, hashPM* dir, TabelaHash* htq, Pessoas* p){
     // Buffers para armazenar os dados lidos de cada linha do arquivo .pm
     char linha[256];
     char comando[5];
@@ -33,7 +38,7 @@ int readFilePM(FILE* arquivoPM, hashPM* dir, Pessoas* p){
     char sobrenome[50];
     char sexo[5];       // String para evitar problemas de leitura com sscanf
     char nasc[20];
-    char cep[5];
+    char cep[25];
     char face[5];       // String para evitar problemas de leitura com sscanf
     char num[10];
     char compl[50];
@@ -45,24 +50,39 @@ int readFilePM(FILE* arquivoPM, hashPM* dir, Pessoas* p){
         sscanf(linha, "%s", comando);
 
         // 1.2: Processa o comando lido do arquivo .pm
+        // 1.3: Comando p - Inserir um habitante
         if(strcmp(comando, "p") == 0){
-            // Comando p - Insere um habitante
             // '*' é usado para ignorar a primeira string (p) e ler os próximos valores
             // Exemplo de linha: comando    cpf      nome  sobrenome  sexo     nasc
-            // Exemplo de linha:   p    12345678901  João  Silva       M    1990-01-01
+            // Exemplo de linha:   p    12345678901  João    Silva     M    1990-01-01
+            // 1.3.1: Lê os dados do habitante (cpf, nome, sobrenome, sexo, nasc) da linha lida do arquivo .pm
             sscanf(linha, "%*s %s %s %s %s %s", cpf, nome, sobrenome, sexo, nasc);
 
-            // 1.2.1: Insere a pessoa na estrutura de dados (Tabela Hash e Pessoas)
+            // 1.3.2: Insere a pessoa na estrutura de dados (Tabela Hash e Pessoas)
             inserirRegPM(dir, cpf, nome, sobrenome, sexo, nasc);
-        }else if(strcmp(comando, "m") == 0){
-            // Comando m - Informa que um dado habitante (cpf) mora num dado endereço (cep,face,num,compl)
+            
+        }
+        // 1.4: Comando m - Informa que um dado habitante (cpf) mora num dado endereço (cep,face,num,compl)
+        else if(strcmp(comando, "m") == 0){
             // '*' é usado para ignorar a primeira string (m) e ler os próximos valores
-            // Exemplo de linha: comando cpf  cep  face  num  compl
-            // Exemplo de linha:   m    12345678901  Q1   10    20    Apt 101
-            sscanf(linha, "%*s %s %s %s %s %s", cpf, cep, face, num, compl);
+            // Exemplo de linha: comando    cpf      cep  face   num    compl
+            // Exemplo de linha:   m    12345678901  Q1    10    20    Apt 101
+            int lidos = sscanf(linha, "%*s %s %s %s %s %s", cpf, cep, face, num, compl);
 
-            // 1.2.2: Adiciona a moradia à pessoa
-            adicionarMoradia(dir, cpf, cep, face, num, compl);
+            // Verificação de segurança: Precisa ler pelo menos o 'm', 'cpf', 'cep', 'face' e 'num'
+            if(lidos >= 5){ 
+                // Se a linha do arquivo .pm não tiver o campo 'compl', o sscanf não vai ler nada para a variável 'compl', e ela vai conter lixo. 
+                // Para evitar problemas de leitura, vamos atribuir um valor padrão para 'compl' quando o campo 'compl' não for fornecido na linha do arquivo .pm.
+                // Se a pessoa não tiver complemento, atribui um valor padrão "-" para evitar problemas de leitura
+                if(lidos == 5) strcpy(compl, "-");
+                Pessoas* p = criarPessoa();
+                Quadras* q = criarQuadra();
+
+                // 1.4.2: Verifica se o CPF e o CEP lidos da linha do arquivo .pm existem nas estruturas de dados (Tabela Hash e Pessoas)
+                if(buscarPessoa(dir, cpf, p) == 0)      printf("[DEBUG-M] ERRO: Nao achei o CPF %s!\n", cpf);
+                else if(buscarQuadra(htq, cep, q) == 0) printf("[DEBUG-M] ERRO: Nao achei a Quadra %s para abrigar o CPF %s!\n", cep, cpf);
+                else                                    adicionarMoradia(dir, cpf, cep, face, num, compl);
+            }
         }
     }
 
@@ -73,7 +93,7 @@ int readFilePM(FILE* arquivoPM, hashPM* dir, Pessoas* p){
 
 
 /*                                       FUNÇÕES PRINCIPAIS                                      */
-int processarPM(Param* param, hashPM* h, Pessoas* p){
+int processarPM(Param* param, hashPM* h, TabelaHash* htq, Pessoas* p){
     // Inicializa o buffer para o caminho completo do arquivo .pm
     char caminhoPM[512];   
 
@@ -100,7 +120,7 @@ int processarPM(Param* param, hashPM* h, Pessoas* p){
     }
 
     // 4: Lê e processa os dados do arquivo .pm
-    if(readFilePM(arquivoPM, h, p) != 0){
+    if(readFilePM(arquivoPM, h, htq, p) != 0){
         fprintf(stderr, "ERRO: Leitura do arquivo .pm.\n");
         fclose(arquivoPM);
         return -1;
--- projeto/src/pm.h
+++ projeto/src/pm.h
@@ -4,34 +4,58 @@
 typedef struct parametro Param;
 
 #include "hashPM.h"
+#include "hashTable.h"
+
+/**
+ * Este módulo é responsável por processar o arquivo .pm, 
+ * que contém informações sobre os habitantes de uma cidade, incluindo seus dados pessoais e suas moradias. 
+ * 
+ * O módulo inclui funções para ler o arquivo .pm, processar os dados contidos nele, 
+ * e armazenar as informações relevantes em uma estrutura de dados adequada (Tabela Hash e Pessoas).
+ */
 
 /*                                       FUNÇÕES AUXILIARES                                      */
 /**
- * Função para montar o caminho completo do arquivo .pm
+ * Esta é uma função auxiliar de processarPM. Ela é responsável por montar o caminho completo do arquivo .pm 
+ * a partir do diretório de entrada e do nome do arquivo .pm fornecidos como parâmetros.
+ * 
+ * O caminho completo do arquivo .pm é armazenado no buffer caminhoPM, 
+ * que deve ser alocado pelo chamador da função processarPM.
+ * 
  * @param param     Ponteiro para a estrutura de parâmetros
  * @param caminhoPM Buffer para armazenar o caminho completo do arquivo .pm
  * @return          0 em caso de sucesso. -1 em caso de erro
  */
 int montarCaminhoPM(Param* param, char* caminhoPM);
 /**
- * Função para ler e processar os dados do arquivo .pm
+ * Esta é uma função auxiliar de processarPM. Ela é responsável por ler o arquivo .pm linha por linha,
+ * processar os comandos e os dados contidos em cada linha, 
+ * e armazenar as informações relevantes em uma estrutura de dados adequada (Tabela Hash e Pessoas).
+ * 
  * @param arquivoPM Ponteiro para o arquivo .pm aberto
  * @param dir       Ponteiro para o diretório da tabela hash onde os dados do arquivo .pm serão armazenados
  * @param p         Ponteiro para a estrutura de Pessoas onde os dados do arquivo .pm serão armazenados
  * @return          0 em caso de sucesso. -1 em caso de erro
  */
-int readFilePM(FILE* arquivoPM, hashPM* dir, Pessoas* p);
+int readFilePM(FILE* arquivoPM, hashPM* dir, TabelaHash* htq, Pessoas* p);
 /*###############################################################################################*/
 
 
 
 /*                                       FUNÇÕES PRINCIPAIS                                      */
 /**
- * Função para processar o arquivo .pm
+ * Esta é uma função wrapper para o processamento do arquivo .pm 
+ * Ela é responsável por coordenar as etapas de processamento do arquivo .pm,
+ * incluindo a montagem do caminho completo do arquivo .pm, a abertura do arquivo para leitura, 
+ * a verificação da estrutura de dados para armazenar os dados do arquivo .pm, 
+ * a leitura e processamento dos dados do arquivo .pm, e o fechamento do arquivo após o processamento.
+ * 
  * @param param Ponteiro para a estrutura de parâmetros
+ * @param h     Ponteiro para o diretório da tabela hash onde os dados do arquivo .pm serão armazenados
+ * @param p     Ponteiro para a estrutura de Pessoas onde os dados do arquivo .pm serão armazenados
  * @return      0 em caso de sucesso. -1 em caso de erro
  */
-int processarPM(Param* param, hashPM* h, Pessoas* p);
+int processarPM(Param* param, hashPM* h, TabelaHash* htq, Pessoas* p);
 /*###############################################################################################*/
 
 
